c3324c44761f917e82743873babf509cc873f653
neazp <andtruazp@gmail.com>
2024-04-29 12:10:19 -0600

    commit 4

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ config()
 
 export const PORT = process.env.PORT || 3002
 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_HOST = process.env.DB_HOST || 'roundhouse.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD|| '123456'
-export const DB_NAME = process.env.DB_NAME || 'db_tareas'
-export const DB_PORT = process.env.DB_PORT || 3307
+export const DB_PASSWORD = process.env.DB_PASSWORD|| 'IkyxGJdUenoSvSWptGgivXMGfwjAIaNC'
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 35578
